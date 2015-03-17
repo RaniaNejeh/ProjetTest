2aa112cfb8bf7b0dc8a03a67ffed34b64ce7f736
Rania NEJEH <rania@Mac-mini-de-Yiming.local>
2015-03-17 15:45:23 +0100

    no message

---
+++ ProjetTest/ProjetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjetTest
+//
+//  Created by Rania NEJEH  on 17/03/2015.
+//  Copyright (c) 2015 Airweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
